a46d4822901268febba4d3d81cdd578b365e77ac
Dirav-2712 <123054093+Dirav-2712@users.noreply.github.com>
2025-09-17 18:19:55 +0530

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,11 @@
+import Navbar from './Navbar'
+
 export default function Layout({ children }) {
   return (
-    <div className="max-w-3xl mx-auto p-6">
-      <header className="mb-10">
-        <h1 className="text-3xl font-bold text-center text-purple-700">
-          Yoga Videos Blog 🧘‍♀️
-        </h1>
-      </header>
-      <main>{children}</main>
-      <footer className="mt-10 text-center text-gray-500">
+    <div>
+      <Navbar />
+      <main className="max-w-4xl mx-auto p-6">{children}</main>
+      <footer className="text-center text-gray-500 mt-10 mb-6">
         © {new Date().getFullYear()} YogaVideos.com
       </footer>
     </div>
