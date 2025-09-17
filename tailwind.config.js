d092e67c5e5c4eb924258060d1a2b6b6827f6358
Dirav-2712 <123054093+Dirav-2712@users.noreply.github.com>
2025-09-17 19:09:22 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./pages/**/*.{js,jsx,mdx}", "./components/**/*.{js,jsx,mdx}"],
-  theme: { extend: {} },
+  content: [
+    "./pages/**/*.{js,jsx,mdx}",       // include MDX pages
+    "./components/**/*.{js,jsx,mdx}",  // include components
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#6B46C1",
+        secondary: "#38A169"
+      },
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"]
+      }
+    },
+  },
   plugins: [],
 }
